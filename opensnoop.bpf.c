64ba2dbc0e5519df93c9da916c7a7dc0d7a606a1
kzeratal <skzeratal@gmail.com>
2026-09-06 16:06:54 +0800

    feat: filter events by executable include map

--- opensnoop.bpf.c
+++ opensnoop.bpf.c
@@ -4,18 +4,30 @@
 
 char LICENSE[] SEC("license") = "GPL";
 
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1);
+	__type(key, char[16]);
+	__type(value, __u8);
+} included_executables SEC(".maps");
+
 SEC("tp/syscalls/sys_enter_openat")
 int trace_openat(struct trace_event_raw_sys_enter *ctx) {
 	char command[16];
-	bpf_get_current_comm(&command, sizeof(command));
-	if (__builtin_memcmp(command, "nvim", 5) != 0) {
+	if (bpf_get_current_comm(&command, sizeof(command)) != 0) {
 		return 0;
 	}
 
-	char filename[256];
-	const char *filename_ptr = (const char *)(void *)ctx->args[1];
-	bpf_probe_read_user_str(filename, sizeof(filename), filename_ptr);
+	if (!bpf_map_lookup_elem(&included_executables, command)) {
+		return 0;
+	}
 
-	bpf_printk("Nvim just opened %s\n", filename);
+	const char *filename_ptr = (const char *)ctx->args[1];
+	char filename[256];
+	if (bpf_probe_read_user_str(filename, sizeof(filename), filename_ptr) < 0) {
+		return 0;
+	}
+
+	bpf_printk("%s attempts to open %s\n", command, filename);
 	return 0;
 }
